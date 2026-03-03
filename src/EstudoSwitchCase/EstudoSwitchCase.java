753d447d79ab2d9cb77b01254b08d12c858763b6
Enzo Rossi Ribeiro da Silva <enzorrsilva@gmail.com>
2026-03-03 03:27:13 -0300

    learning the java`s switch cases

---
+++ src/EstudoSwitchCase/EstudoSwitchCase.java
@@ -0,0 +1,43 @@
+package EstudoSwitchCase;
+
+import java.util.Scanner;
+
+public class EstudoSwitchCase {
+    public static void main(String[] args) {
+        /*
+        *
+        * SwitchCases: Que servem para gerar casos especificos
+        * Objetivo: Pedir pro usuario escolher entre os Ninjas
+        * switchCase
+        * */
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Opcoes para o usuario escolher
+        System.out.println("Escolha um personagem: ");
+        System.out.println("1 - Naruto Uzumaki");
+        System.out.println("2 - Sasuke Uchiha");
+        System.out.println("3 - Sakura Haruno");
+
+        //Pedir para o usuario escolher uma das opcoes
+        int escolhaDoUsuario = scanner.nextInt();
+
+        System.out.println("Voce digitou o numero: " + escolhaDoUsuario);
+
+        //Reacao ao escolher um personagem
+
+        switch (escolhaDoUsuario){
+            case 1:
+                System.out.println("O usuario escolheu o Naruto Uzumaki");
+                break;
+            case 2:
+                System.out.println("O usuario escolheu o Sasuke Uchiha");
+                break;
+            case 3:
+                System.out.println("O usuario escolheu a Sakura Haruno");
+                break;
+            default:
+                System.out.println("Você não digitou uma resposta valida, tente novamente!");
+        }
+    }
+}
